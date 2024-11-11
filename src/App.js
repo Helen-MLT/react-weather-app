9234bc44d9a73591d791eb996a664b1f959763d0
Helen-MLT <h.milletti@gmail.com>
2024-11-11 14:38:41 +0100

    Added Github repo url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,22 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/Helen-MLT" target="_blank">
+          Hélèna Milletti
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/Helen-MLT/react-weather-app"
+          target="_blank"
+        >
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }
