6f67ce26982b738ab57aa5232778c0002236ee1c
Helen-MLT <h.milletti@gmail.com>
2024-11-11 15:35:42 +0100

    Added bootstrap to project

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/Helen-MLT" target="_blank">
-          Hélèna Milletti
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/Helen-MLT/react-weather-app"
-          target="_blank"
-        >
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/Helen-MLT" target="_blank">
+            Hélèna Milletti
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/Helen-MLT/react-weather-app"
+            target="_blank"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
