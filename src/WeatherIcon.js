52a5a2d8946ae23b3f4b59b9147b2b020bfdfab7
Helen-MLT <h.milletti@gmail.com>
2024-11-16 17:55:16 +0100

    Added animated react icons

---
+++ src/WeatherIcon.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactAnimatedWeather from "react-animated-weather";
+
+export default function WeatherIcon(props) {
+  console.log(props.code);
+  const codeMapping = {
+    "clear sky": "CLEAR_DAY",
+    "clear sky": "CLEAR_NIGHT",
+    "thunderstorm-day": "RAIN",
+    "snow-day": "SNOW",
+    "few clouds": "PARTLY_CLOUDY_DAY",
+    "few clouds": "PARTLY_CLOUDY_NIGHT",
+    "scattered clouds": "PARTLY_CLOUDY_DAY",
+    "scattered clouds": "PARTLY_CLOUDY_NIGHT",
+    "broken clouds": "CLOUDY",
+    "broken clouds": "CLOUDY",
+    "shower rain": "RAIN",
+    "shower rain": "RAIN",
+    "mist-day": "FOG",
+  };
+
+  return (
+    <ReactAnimatedWeather
+      icon={codeMapping[props.code]}
+      color="black"
+      size={64}
+      animate={true}
+    />
+  );
+}
