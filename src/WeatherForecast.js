06fb5c08c31211c1bb12ce45c2e1a170d5f30215
Helen-MLT <h.milletti@gmail.com>
2024-11-18 16:14:01 +0100

    API call + coordinates

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,19 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  let apiKey = "b6c516da5eaaa3o3f0cb04t9c962c4a1";
+  let latitude = props.coordinates.latitude;
+  let longitude = props.coordinates.longitude;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lat=${latitude}&lon=${longitude}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
   return (
     <div className="WeatherForecast">
       <div className="row">
