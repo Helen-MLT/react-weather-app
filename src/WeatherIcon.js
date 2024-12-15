3ca5b3e8512731a8aea1b25d8ed54cc02fcea027
Helen-MLT <h.milletti@gmail.com>
2024-12-15 17:13:32 +0100

    Update WeatherIcon.js

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -5,8 +5,10 @@ export default function WeatherIcon(props) {
   console.log(props.code);
   const codeMapping = {
     "sky is clear": "CLEAR_DAY",
+    "clear sky": "CLEAR_DAY",
     "thunderstorm-day": "RAIN",
     snow: "SNOW",
+    "light snow": "SNOW",
     "rain and snow": "SNOW",
     "few clouds": "PARTLY_CLOUDY_DAY",
     "scattered clouds": "PARTLY_CLOUDY_DAY",
@@ -15,6 +17,7 @@ export default function WeatherIcon(props) {
     "shower rain": "RAIN",
     "light rain": "RAIN",
     "heavy intensity rain": "RAIN",
+    "light intensity drizzle": "RAIN",
     "moderate rain": "RAIN",
     mist: "FOG",
   };
